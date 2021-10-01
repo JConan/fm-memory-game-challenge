4465900bf58737be0806f560f8ef6357060b5e77
Jconan <conanjahh@hotmail.com>
2021-10-01 23:39:04 +0200

    implement Timer hooks

---
+++ src/hooks/Timer/index.test.tsx
@@ -0,0 +1,41 @@
+import { act, renderHook } from "@testing-library/react-hooks";
+import { useTimer } from ".";
+
+describe("hook for Timer", () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  it("should start with 0:00", async () => {
+    const { result } = renderHook(() => useTimer());
+    expect(result.current.value).toBe("0:00");
+
+    act(() => {
+      jest.advanceTimersByTime(3000);
+    });
+    expect(result.current.value).toBe("0:03");
+
+    act(() => {
+      jest.advanceTimersByTime(90 * 1000);
+    });
+    expect(result.current.value).toBe("1:33");
+  });
+
+  it("should able to stop/start", async () => {
+    const { result } = renderHook(() => useTimer());
+    expect(result.current.value).toBe("0:00");
+
+    act(() => {
+      jest.advanceTimersByTime(3000);
+      result.current.stop();
+      jest.advanceTimersByTime(3000);
+      result.current.start();
+      jest.advanceTimersByTime(1000);
+    });
+
+    expect(result.current.value).toBe("0:04");
+  });
+});
---
+++ src/hooks/Timer/index.ts
@@ -0,0 +1,31 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import { createTimer } from "libraries/Timer";
+
+export const useTimer = () => {
+  const isMounted = useRef(true);
+  const [value, setvalue] = useState("0:00");
+
+  const { start, stop } = createTimer({
+    tickInterval: 1000,
+    onTick: useCallback((count) => {
+      const seconds = `${count % 60}`.padStart(2, "0");
+      const minutes = Math.floor(count / 60);
+      isMounted.current && setvalue(`${minutes}:${seconds}`);
+    }, []),
+  });
+
+  useEffect(() => {
+    start();
+    return () => {
+      isMounted.current = false;
+      stop();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return {
+    value,
+    start,
+    stop,
+  };
+};
