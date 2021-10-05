7f5df4d5d7f1047c25732831ed5c79f391c45471
Jconan <conanjahh@hotmail.com>
2021-10-05 23:52:27 +0200

    implement data Class SoloGameScore
    it is used for storing moves counter and elapsed time

---
+++ src/data/SoloGameScore.test.ts
@@ -0,0 +1,33 @@
+import { SoloGameScore } from "./SoloGameScore";
+
+describe("Class SoloGameScore for storing moves counter and elapsed time", () => {
+  it("should be have initial value", () => {
+    const score = new SoloGameScore();
+
+    expect(score.move.getCount()).toEqual(0);
+    expect(score.timer.toString()).toEqual("0:00");
+  });
+
+  it("should be able to increment counter", () => {
+    const score = new SoloGameScore();
+
+    score.move.add();
+    expect(score.move.getCount()).toEqual(1);
+
+    for (let i = 0; i < 10; i++) {
+      score.move.add();
+    }
+    expect(score.move.getCount()).toEqual(11);
+  });
+
+  it("should be able to have a working timer", () => {
+    jest.useFakeTimers();
+    const score = new SoloGameScore();
+
+    score.timer.start();
+    jest.advanceTimersByTime(93000);
+    expect(score.timer.toString()).toBe("1:33");
+
+    jest.useRealTimers();
+  });
+});
---
+++ src/data/SoloGameScore.ts
@@ -0,0 +1,31 @@
+import { Timer, createTimer } from "libraries/Tools";
+
+export interface SoloGameScore {
+  move: {
+    getCount: () => number;
+    add: () => void;
+  };
+  timer: Timer & {
+    toString: () => string;
+  };
+}
+
+export class SoloGameScore {
+  private counter: number = 0;
+
+  constructor() {
+    this.move = {
+      getCount: () => this.counter,
+      add: () => this.counter++,
+    };
+
+    this.timer = {
+      ...createTimer({ tickInterval: 1000 }),
+      toString: () => {
+        const seconds = `${this.timer.getTickCount() % 60}`.padStart(2, "0");
+        const minutes = Math.floor(this.timer.getTickCount() / 60);
+        return `${minutes}:${seconds}`;
+      },
+    };
+  }
+}
