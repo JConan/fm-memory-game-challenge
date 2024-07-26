b5bcad20c6f568c433a518fe5babbf2de4e1ce60
jconan <conanjahh@hotmail.com>
2024-07-26 03:54:46 +0200

    add github basepath

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
